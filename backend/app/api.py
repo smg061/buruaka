0201a7c1a316c1572ca8085c5d13e8ffedfdaaa9
smg061 <max.go.95@gmail.com>
2023-08-26 19:25:13 -0500

    config stuff

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 
 from app.routes.router import router as api_router
+from app.config import app_configs
 
-app = FastAPI()
+app = FastAPI(**app_configs)
 
 app.include_router(api_router, prefix="/api/v1")
---
+++ backend/app/config.py
@@ -0,0 +1,61 @@
+from typing import Any
+from enum import Enum
+from pydantic import PostgresDsn, root_validator
+from pydantic_settings import BaseSettings
+
+
+class Environment(str, Enum):
+    LOCAL = "LOCAL"
+    STAGING = "STAGING"
+    TESTING = "TESTING"
+    PRODUCTION = "PRODUCTION"
+
+    @property
+    def is_debug(self) -> bool:
+        return self in (self.LOCAL, self.STAGING, self.TESTING)
+
+    @property
+    def is_testing(self) -> bool:
+        return self == self.TESTING
+
+    @property
+    def is_deployed(self) -> bool:
+        return self in (self.PRODUCTION, self.STAGING)
+
+
+class Settings(BaseSettings):
+    DATABASE_URL: PostgresDsn
+
+    SITE_DOMAIN: str = "localhost:8000"
+
+    ENVIRONMENT: Environment = Environment.LOCAL
+
+    SENTRY_DSN: str | None = None
+
+    CORS_ORIGINS: list[str]
+    CORS_ORIGINS_REGEX: str | None
+    CORS_HEADERS: list[str]
+
+    APP_VERSION: str = "1"
+
+    class Config:
+        env_file = ".env"
+        extra = "ignore"
+
+    @root_validator(skip_on_failure=True)
+    def validate_sentry_non_local(cls, data: dict[str, Any]) -> dict[str, Any]:
+        if data.get("ENVIRONMENT").is_deployed and not data.get("SENTRY_DSN"):
+            raise ValueError("SENTRY_DSN is required for deployed environments")
+        return data
+
+
+settings = Settings()
+
+app_configs: dict[str, Any] = {
+    "title": "API",
+}
+if settings.ENVIRONMENT.is_deployed:
+    app_configs["root_path"] = f"v/{settings.APP_VERSION}"
+
+if not settings.ENVIRONMENT.is_debug:
+    app_configs["openapi_url"] = None  # hide docs
